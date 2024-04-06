e81ed24f7dadcf8238a496583cc85599ef60d6a1
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-04-06 18:26:05 -0500

    feat: Add more sophisticated simulation function
    The function now takes into account time step, and the "corrective"
    vector is mixed in with the destination vector in a different way.

--- src/simulate.c
+++ src/simulate.c
@@ -1,18 +1,23 @@
 #include <entity.h>
+#include <game.h>
 #include <raylib.h>
 #include <raymath.h>
 
-void simulate(Camera *camera, Entity *entities, int count) {
+extern Game game;
+
+void simulate(Camera *camera, Entity *entities, int count, double dt) {
+  game.elapsedTime += dt;
+
   for (int i = 0; i < count; i++) {
     Entity *e = &entities[i];
 
-    e->vel.x += e->acc.x;
-    e->vel.y += e->acc.y;
-    e->vel.z += e->acc.z;
+    e->vel.x += e->acc.x * dt;
+    e->vel.y += e->acc.y * dt;
+    e->vel.z += e->acc.z * dt;
 
-    e->pos.x += e->vel.x;
-    e->pos.y += e->vel.y;
-    e->pos.z += e->vel.z;
+    e->pos.x += e->vel.x * dt;
+    e->pos.y += e->vel.y * dt;
+    e->pos.z += e->vel.z * dt;
 
     if (e->moveOrder) {
       Vector3 position = (Vector3){e->pos.x, e->pos.y, e->pos.z};
@@ -21,10 +26,22 @@ void simulate(Camera *camera, Entity *entities, int count) {
 
       Vector3 heading = Vector3Normalize(velocity);
       Vector3 desired = Vector3Normalize(Vector3Subtract(target, position));
+
       float dot = Vector3DotProduct(heading, desired);
       Vector3 along = Vector3Scale(Vector3Normalize(desired), dot);
+
       Vector3 corrective = Vector3Normalize(Vector3Subtract(along, heading));
-      Vector3 acc = Vector3Add(corrective, desired);
+
+      Vector3 acc;
+      if (Vector3Length(Vector3Subtract(heading, desired)) > 0.1) {
+        acc = Vector3Add(Vector3Scale(corrective, 10), desired);
+      } else {
+        acc = Vector3Add(Vector3Scale(desired, 10), corrective);
+      }
+
+      double maxAcc = 9.81; // 1g
+
+      acc = Vector3Scale(Vector3Normalize(acc), maxAcc);
 
       e->acc.x = acc.x;
       e->acc.y = acc.y;
